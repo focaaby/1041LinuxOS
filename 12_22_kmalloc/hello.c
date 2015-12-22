f26789d2e8d8dc73b25be025efb71b5e02a9403f
Jerry Wang <focaaby@gmail.com>
2015-12-22 17:04:25 +0800

    added kmalloc module

---
+++ 12_22_kmalloc/hello.c
@@ -0,0 +1,44 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("GPL");
+
+
+struct student {
+	int age;
+	char name[10];
+};
+
+struct student *ptr;
+struct student *ptr_tmp;
+ 
+static int __init hello_init(void) {
+	int i;
+	int myage = 22;
+	char myname[] = "MaoLin"; 
+	printk(KERN_INFO "MAOLIN_Hello\n");
+	ptr = kmalloc(sizeof(struct student), GFP_KERNEL);
+	ptr_tmp = ptr;
+	printk(KERN_INFO "MAOLIN, And this pages address:0x%lx\n",(unsigned long)ptr);
+	
+	if(ptr != NULL) {
+		for(i = 0; i < 10; i++) {
+			ptr->age = myage + i;
+			strcpy(ptr->name, myname);
+			printk(KERN_INFO "MAOLIN, age:%d, name: %s", ptr->age, ptr->name);
+			printk("%d\n", i);
+			ptr++;
+		}
+	}
+        return 0;
+}
+
+static void __exit hello_exit(void) {
+	printk(KERN_INFO "MAOLIN_Goodbye\n");
+	kfree(ptr_tmp);
+}
+ 
+module_init(hello_init);
+module_exit(hello_exit);
